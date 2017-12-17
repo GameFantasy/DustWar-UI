63c631c1615fa47d345426cab94cfb859a5621b6
Jason <1561142287@qqcom>
2017-12-17 16:47:28 +0800

    第一次测试
    第一次测试

---
+++ DustWar/Project/Dustys/Assets/Scripts/Start/StartPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartPanel : MonoBehaviour {
+    private Button btnFight;
+    private Button btnSingle;
+    private Button btnSetting;
+    private Button btnExitGame;
+	// Use this for initialization
+	void Start () {
+        transform.Find("Figth").GetComponent<Button>().onClick.AddListener(ClickFight);
+        transform.Find("Single").GetComponent<Button>().onClick.AddListener(ClickSingle);
+        transform.Find("Setting").GetComponent<Button>().onClick.AddListener(ClickSetting);
+        transform.Find("Exit").GetComponent<Button>().onClick.AddListener(ClickExit);	
+	}
+    void ClickFight()
+    { 
+    }
+    void ClickSingle()
+    { 
+    }
+    void ClickSetting()
+    { 
+    }
+    void ClickExit()
+    {
+        Application.Quit();
+    }
+}
